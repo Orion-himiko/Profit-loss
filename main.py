cad7ba4402a9dd152a8d09718ce4a452d0a1c011
Orion-himiko <prishah.shah0707@gmail.com>
2024-12-30 21:28:02 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+SP=int(input("Enter Selling Price: "))
+CP=int(input("Enter Cost Price: "))
+
+Profit=SP-CP
+
+if (Profit>0):
+    print("The profit vale is:",Profit)
+
+else:
+    print("Loss is made")    
+
